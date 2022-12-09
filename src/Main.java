0fd93cb22c29323d5989aa2ad22bcba730015fd7
Expo <crdailey01@gmail.com>
2022-12-08 19:53:29 -0800

    no new concepts. Less complex with no hiding of component methods.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        //creation of component objects ^^
+
+        Bedroom bedRoom = new Bedroom("Ryan", wall1, wall2, wall3, wall4, ceiling,bed, lamp);
+
+        //compilation of component objects into 'master class' ^^
+
+        bedRoom.makeBed();
+        bedRoom.getLamp().turnOn();
+        bedRoom.name();
+    }
+
+
+}
